46bcd51c1350bd724f1d5d1d58d8a18c1670bcd0
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-04 22:09:06 +0200

    feat: end point for image upload

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const app = express();
 
 app.use(fileUpload());
 
+// end point
+
+app.post('/upload', (req, res) => {
+  if(req.files === null){
+    return res.status(400).json({msg: 'No file uploaded!'})
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/upload/${file.name}`, err => {
+    if(err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+  })
+
+})
